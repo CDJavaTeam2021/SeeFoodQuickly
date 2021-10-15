ec88e5533d5efb682a097346d4f6c71b57f0e3e7
Stuart-Yee <ki6fzb@gmail.com>
2021-10-14 17:06:46 -0700

    created data models

---
+++ SeeFoodQuickly/src/main/java/com/seefoodquickly/models/Item.java
@@ -0,0 +1,122 @@
+package com.seefoodquickly.models;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="items")
+public class Item {
+
+	
+	
+	//Relationship attributes
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(updatable=false)
+	private Date createdAt;
+	
+	private Date updatedAt;
+	
+	private float lineTotal;
+	
+	private int quantity;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name="order_id", updatable = true)
+	private Order parentOrder;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name="product_id", updatable=true)
+	private Product itemProduct;
+
+	@PrePersist
+	protected void onCreate() {
+		this.createdAt = new Date();
+	}
+	
+	@PreUpdate
+	protected void onUpdate() {
+		this.updatedAt = new Date();
+	}
+	
+	//Empty constructor
+	
+	public Item() {
+	}
+
+	
+	//getters and setters
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+	public float getLineTotal() {
+		return lineTotal;
+	}
+
+	public void setLineTotal(float lineTotal) {
+		this.lineTotal = lineTotal;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public Order getParentOrder() {
+		return parentOrder;
+	}
+
+	public void setParentOrder(Order parentOrder) {
+		this.parentOrder = parentOrder;
+	}
+
+	public Product getItemProduct() {
+		return itemProduct;
+	}
+
+	public void setItemProduct(Product itemProduct) {
+		this.itemProduct = itemProduct;
+	}
+	
+	
+	
+
+}
+
---
+++ SeeFoodQuickly/src/main/java/com/seefoodquickly/models/Order.java
@@ -0,0 +1,164 @@
+package com.seefoodquickly.models;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="orders")
+public class Order {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(updatable=false)
+	private Date createdAt;
+	
+	private Date updatedAt;
+	
+	private String orderNumber;
+	
+	private Boolean orderOpen;
+	
+	private float subtotal;
+	
+	private float tax;
+	
+	private float total;
+	
+	private String status;
+	
+	//relationship attributes
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name="user_id", updatable = true)
+	private User customer;
+	
+	@OneToMany(mappedBy="parentOrder", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	private List<Item> items;
+	
+	
+	@PrePersist
+	protected void onCreate() {
+		this.createdAt = new Date();
+	}
+	
+	@PreUpdate
+	protected void onUpdate() {
+		this.updatedAt = new Date();
+	}
+	
+	//Empty Constructor
+	public Order() {
+	}
+		
+	//Getters and setters
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+	public String getOrderNumber() {
+		return orderNumber;
+	}
+
+	public void setOrderNumber(String orderNumber) {
+		this.orderNumber = orderNumber;
+	}
+
+	public Boolean getOrderOpen() {
+		return orderOpen;
+	}
+
+	public void setOrderOpen(Boolean orderOpen) {
+		this.orderOpen = orderOpen;
+	}
+
+	public float getSubtotal() {
+		return subtotal;
+	}
+
+	public void setSubtotal(float subtotal) {
+		this.subtotal = subtotal;
+	}
+
+	public float getTax() {
+		return tax;
+	}
+
+	public void setTax(float tax) {
+		this.tax = tax;
+	}
+
+	public float getTotal() {
+		return total;
+	}
+
+	public void setTotal(float total) {
+		this.total = total;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public User getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(User customer) {
+		this.customer = customer;
+	}
+
+	public List<Item> getItems() {
+		return items;
+	}
+
+	public void setItems(List<Item> items) {
+		this.items = items;
+	}
+	
+	
+	
+		
+	
+
+}
